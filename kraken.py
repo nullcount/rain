17f2b09a81217a158615dfa9df895f4ddd1bd070
Count Null <70529195+count-null@users.noreply.github.com>
2022-11-12 23:58:39 -0500

    add kraken recent widthdraws

--- kraken.py
+++ kraken.py
@@ -27,8 +27,8 @@ class Kraken:
         return sigdigest.decode()
 
     def check_errors(self, response, payload, endpoint):
-        if response['errors']:
-            for err in response['errors']:
+        if response['error']:
+            for err in response['error']:
                 self.log.error("kraken responded with error: {}".format(err))
             self.log.error('using payload: {}'.format(payload))
             self.log.error('from : {}'.format(endpoint))
@@ -46,9 +46,9 @@ class Kraken:
             (self.api_url + uri_path),
             headers=headers,
             data=data
-        )
+        ).json()
         self.check_errors(req, data, uri_path)
-        return req
+        return req['result']
 
     def get_onchain_address(self):
         payload = {
@@ -56,8 +56,8 @@ class Kraken:
             "asset": "XBT",
             "method": "Bitcoin",
         }
-        res = self.kraken_request('/0/private/DepositAddresses', payload).json()
-        addr = res['result'][0]['address']
+        res = self.kraken_request('/0/private/DepositAddresses', payload)
+        addr = res[0]['address']
         self.log.info("kraken deposit address: {}".format(addr))
         return addr
 
@@ -79,26 +79,36 @@ class Kraken:
             "key": self.widthdraw_key,
             "amount": sats / COIN_SATS
         }
-        res = self.kraken_request('/0/private/WithdrawInfo', payload).json()['result']
+        res = self.kraken_request('/0/private/WithdrawInfo', payload)
+        print(res)
         fee_quote = {
             'amount': int(float(res['amount']) * COIN_SATS),
             'fee': int(float(res['fee']) * COIN_SATS)
         }
-        self.log.info("kraken fee: {} sats widthdrawl amount: {} sats".format(fee_quote['fee'], sats))
+        self.log.info("kraken fee: {} sats widthdraw amount: {} sats".format(fee_quote['fee'], sats))
         return fee_quote
+
+    def get_pending_widthdraw_sats(self):
+        widthdraws = self.get_recent_widthdraws()
+        pending_amt = 0
+        for w in widthdraws:
+            if w['status'] in ['Initial', 'Pending']:
+                pending_amt += int(float(w['amount']) * COIN_SATS)
+                self.log.info('kraken [{}] widthdraw #{} of {} sats'.format(w['status'].lower(), w['refid'], w['amount']))
+        return pending_amt
 
     def get_recent_widthdraws(self):
         payload = {
             "nonce": str(int(1000*time.time())),
             "asset": "XBT"
         }
-        res = self.kraken_request('/0/private/WithdrawStatus', payload).json()
+        res = self.kraken_request('/0/private/WithdrawStatus', payload)
         return res
 
     def get_account_balance(self):
         payload = {"nonce": str(int(1000*time.time()))}
-        res = self.kraken_request('/0/private/Balance', payload).json()
-        balance = int(float(res['result']['XXBT']) * COIN_SATS)
+        res = self.kraken_request('/0/private/Balance', payload)
+        balance = int(float(res['XXBT']) * COIN_SATS)
         self.log.info("kraken account balance: {} sats".format(balance))
         return balance
 
--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def main():
     LOOP_PUB = "021c97a90a411ff2b10dc2a8e32de2f29d2fa49d41bfbb52bd416e460db0747d0d"
     KRAKEN_PUB = "02f1a8c87607f415c8f22c00593002775941dea48869ce23096af27b0cfdcc0b69"
 
-    CHAN_CAP_SATS = 25_000_000 # capacity of LOOP and Kraken channel
-    RAIN_AMT_SATS = 16_000_000 # amount to loop-out through Kraken
+    CHAN_CAP_SATS = 24_000_000 # capacity of LOOP and Kraken channel
     MIN_ONCHAIN_BALANCE = 200_000 # maintain at least this much in the onchain wallet
 
     LOOP_CHAN_CONFIG = {
@@ -29,6 +28,11 @@ def main():
     all_chans = lnd.get_channels()
     loop_chan = lnd.has_channel_with(LOOP_PUB)
     kraken_chan = lnd.has_channel_with(KRAKEN_PUB)
+    
+    confirmed = lnd.get_onchain_balance()
+    unconfirmed = lnd.get_unconfirmed_balance()
+    kraken_balance = kraken.get_account_balance()
+    kraken_pending_widthdraw_sats = kraken.get_pending_widthdraw_sats()    
 
     if loop_chan:
         if kraken_chan.local_balance >= kraken_chan.capactiy * 0.8:
@@ -37,22 +41,22 @@ def main():
             # need to get the max_htlc for the channel
             #    and loop multiple deposits to kraken below the max_htlc
     else:
-        print(all_chans)
-        return 0
-        confirmed = lnd.get_onchain_balance()
-        unconfirmed = lnd.get_unconfirmed_balance()
-        kraken_balance = kraken.get_account_balance()
+        if kraken_pending_widthdraw_sats:
+            log.info("waiting for kraken widthdraw(s) to process: {} sats total".format(kraken_pending_widthdraw_sats))
+            return 1
         if confirmed >= CHAN_CAP_SATS + MIN_ONCHAIN_BALANCE:
-            # 
             lnd.open_channel(LOOP_CHAN_CONFIG)
-        if kraken_balance >= RAIN_AMT_SATS:
+            return 1
+        if kraken_balance >= CHAN_CAP_SATS:
             fee = kraken.get_widthdraw_info(kraken_balance)['fee']
             if fee <= 1000: # expected flat fee for kraken BTC widthdraws
                 kraken.widthdraw_onchain(kraken_balance)
             else:
                 log.warning("kraken widthdraw fee ({} sats) larger than expected".format(fee))
+            return 1
         if unconfirmed + confirmed >= CHAN_CAP_SATS + MIN_ONCHAIN_BALANCE:
             log.info("waiting for {} unconfirmed sats to take action".format(unconfirmed))
+            return 1
 
 
 if __name__ == "__main__":

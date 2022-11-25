ff2c32c2c92380158a94410de6d0b944922d3629
Vincent Davis <vincentmdavis@protonmail.com>
2022-11-24 19:50:23 -0500

    use info level instead

--- notify.py
+++ notify.py
@@ -22,7 +22,7 @@ class Logger:
     def __init__(self, filename):
         logging.basicConfig(filename=filename, filemode='a', format='[%(asctime)s] %(levelname)-8s %(message)s') 
         self.log = logging.getLogger()
-        self.log.setLevel(logging.DEBUG)
+        self.log.setLevel(logging.INFO)
 
     def info(self, message):
         self.log.info(message)

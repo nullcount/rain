a8c7ce50f218bfe07b14b7c1cfbe169de40fc2ac
count-null <70529195+count-null@users.noreply.github.com>
2022-12-03 13:27:05 -0500

    fix

--- report.py
+++ report.py
@@ -1,5 +1,12 @@
+import time
+
+
 class Report:
     def __init__(self, report_config, node, log):
         self.node = node
         self.log = log
-        
+        self.interval = report_config['interval']
+
+    def mainLoop(self):
+        while True:
+            time.sleep(512)

7dfd03dd02dae321bad61805914d9283a7fc6170
count-null <70529195+count-null@users.noreply.github.com>
2022-12-04 19:20:04 -0500

    test

--- report.py
+++ report.py
@@ -2,7 +2,8 @@ import time
 import schedule
 import os
 from multiprocessing import Process
-
+import requests
+import json
 
 class Report:
     def __init__(self, report_config, node, log):
@@ -13,7 +14,16 @@ class Report:
         self.expensive_nodes = []
 
     def get_profit_loss(self):
-        return
+        req = requests.get('http://localhost:8889/api/invoices/?format=json', auth=("lndg-admin", "163f0f6988f7ba4eb065130db5ab591a9f81f12e9f56468350be2215147a493e"))
+        print(req.content)
+    
+    def check_node_is_expensive(self, nodes):
+        for node in nodes:
+            fees = self.node.get_node_fee_report(node.pub_key)
+            if fees['in_corrected_avg'] and fees['in_corrected_avg']> 1000 and fees['capacity'] > 100_000_000:
+               self.expensive_nodes.append(fees)
+               print(self.expensive_nodes)
+
 
    
     def get_expensive_nodes(self):
@@ -23,18 +33,11 @@ class Report:
             for i in range(0, n):
                 yield l[i::n]
 
-        def check_node_is_expensive(nodes):
-            for node in nodes:
-                fees = self.node.get_node_fee_report(node.pub_key)
-                if fees and fees['in_corrected_avg']:
-                    if fees['in_corrected_avg'] > 1000 and fees['capacity'] > 100_000_000:
-                        self.expensive_nodes.append(fees)
-
         g = self.node.get_graph(refresh=True)
         thread_pool = []
         chunky = chunks(g.nodes, os.cpu_count() - 1)
         for chunk in chunky:
-            t = Process(target=check_node_is_expensive, kwargs={"nodes":chunk})
+            t = Process(target=self.check_node_is_expensive, kwargs={"nodes":chunk})
             t.start()
             thread_pool.append(t)
         for thread in thread_pool:
@@ -48,9 +51,11 @@ class Report:
 
     def make_report(self):
         expensive = self.get_expensive_nodes()
+        print(expensive)
         for n in expensive:
             print(self.node.get_node_alias(n['pub_key']))
             print(n['in_corrected_avg'])
+        #self.get_profit_loss()
         return
 
     def mainLoop(self):

570c861e9726f06c82960d15999c5dfbb7b79297
count-null <70529195+count-null@users.noreply.github.com>
2023-09-22 22:02:22 -0400

    add: types for LND objects

--- base.py
+++ base.py
@@ -1,5 +1,5 @@
 from result import Result
-from typing import Callable
+from typing import Callable, List
 from box import Box
 
 class AdminNotifyService:
@@ -13,28 +13,141 @@ class AdminNotifyService:
     
     def await_confirm(self, prompt: str, callback: Callable) -> Result[None, str]:
         raise NotImplementedError
-    
+
+class OpenChannelRequest:
+    """
+    Used in BitcoinLightningNode.open_channel() 
+    """
+    def __init__(
+            self, 
+            peer_pubkey: str, 
+            channel_capacity: int, 
+            base_fee: int,
+            ppm_fee: int,
+            cltv_delta: int,
+            min_htlc_sats: int,
+            vbyte_sats: int,
+            is_spend_unconfirmed: bool,
+            is_unannounced: bool,
+        ) -> None:
+        self.peer_pubkey = peer_pubkey
+        self.channel_capacity = channel_capacity
+        self.base_fee = base_fee
+        self.ppm_fee = ppm_fee
+        self.cltv_delta = cltv_delta
+        self.min_htlc_sats = min_htlc_sats
+        self.vbyte_sats = vbyte_sats
+        self.is_spend_unconfirmed = is_spend_unconfirmed
+        self.is_unannounced = is_unannounced
+
+class CloseChannelRequest:
+    """
+    Used in BitcoinLightningNode.close_channel() 
+    """
+    def __init__(
+            self, 
+            channel_point: str, 
+            vbyte_sats: int,
+            is_force: bool,
+        ) -> None:
+        self.channel_point = channel_point
+        self.vbyte_sats = vbyte_sats
+        self.is_force = is_force
+
+class PayInvoiceRequest:
+    """
+    Used in BitcoinLightningNode.pay_invoice() 
+    """
+    def __init__(
+            self,
+            outgoing_channel_id: str,
+            invoice: str,
+            fee_limit_sats: int
+    ) -> None:
+        self.outgoing_channel_id = outgoing_channel_id
+        self.invoice = invoice
+        self.fee_limit_sats = fee_limit_sats
+
+class PendingOpenChannel:
+    """
+    Used in BitcoinLightningNode.get_pending_open_channels() 
+    """
+    def __init__(
+            self,
+            peer_pubkey: str,
+            channel_point: str,
+            capacity: int,
+            local_balance: int,
+            initiator: str,
+            private: bool,
+    ) -> None:
+        self.peer_pubkey = peer_pubkey
+        self.channel_point = channel_point
+        self.capacity = capacity
+        self.local_balance = local_balance
+        self.initiator = initiator
+        self.private = private
+
+class ActiveOpenChannel:
+    """
+    Used in BitcoinLightningNode.get_open_channels() 
+    """
+    def __init__(
+            self,
+            active: bool,
+            channel_id: str,
+            peer_pubkey: str,
+            channel_point: str,
+            capacity: int,
+            local_balance: int,
+            initiator: str,
+            private: bool,
+    ) -> None:
+        self.active = active
+        self.channel_id = channel_id
+        self.peer_pubkey = peer_pubkey
+        self.channel_point = channel_point
+        self.capacity = capacity
+        self.local_balance = local_balance
+        self.initiator = initiator
+        self.private = private
+
+class DecodedInvoice:
+    """
+    Used in BitcoinLightningNode.decode_invoice() 
+    """
+    def __init__(
+        self,
+        dest_pubkey: str,
+        payment_hash: str,
+        amount_sats: int,
+        description: str,
+    ) -> None:
+        self.dest_pubkey = dest_pubkey
+        self.payment_hash = payment_hash
+        self.amount_sats = amount_sats
+        self.description = description    
 
 class BitcoinLightingNode:
     """
     Extend with gRPC or API calls to a node
     """
-    def open_channel(self) -> Result[None, str]:
+    def open_channel(self, req: OpenChannelRequest) -> Result[None, str]:
         raise NotImplementedError
     
-    def close_channel(self) -> Result[None, str]:
+    def close_channel(self, req: CloseChannelRequest) -> Result[None, str]:
         raise NotImplementedError
     
-    def get_pending_channels(self) -> Result[Box, str]:
+    def get_pending_open_channels(self) -> Result[List[PendingOpenChannel], str]:
         raise NotImplementedError
     
-    def get_opened_channels(self) -> Result[Box, str]:
+    def get_opened_channels(self) -> Result[List[ActiveOpenChannel], str]:
         raise NotImplementedError
     
     def get_invoice(self, sats: int) -> Result[str, str]:
         raise NotImplementedError
     
-    def pay_invoice(self) -> Result[None, str]:
+    def pay_invoice(self, req: PayInvoiceRequest) -> Result[None, str]:
         raise NotImplementedError
     
     def get_address(self) -> Result[str, str]:
@@ -49,7 +162,7 @@ class BitcoinLightingNode:
     def get_confirmed_balance(self) -> Result[int, str]:
         raise NotImplementedError
     
-    def decode_invoice(self, invoice: str) -> Result[Box, str]:
+    def decode_invoice(self, invoice: str) -> Result[DecodedInvoice, str]:
         raise NotImplementedError
     
     def sign_message(self, message: str) -> Result[str, str]:

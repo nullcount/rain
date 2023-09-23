325fcdfcf1b41b11758740f13d884f9d797a6b99
davisv7 <vincentmdavis@protonmail.com>
2023-09-22 22:37:49 -0400

    open_channel, add_peer, close_channel, get_pending_open_channels, get_opened_Channels, typing

--- lnd.py
+++ lnd.py
@@ -5,9 +5,10 @@ from grpc_generated import lightning_pb2_grpc as lnrpc, lightning_pb2 as ln
 from grpc_generated import router_pb2_grpc as routerrpc, router_pb2 as router
 from const import MESSAGE_SIZE_MB
 import config
-from typing import Any
-from base import BitcoinLightingNode
+from typing import Any,List
+from base import BitcoinLightingNode,OpenChannelRequest,CloseChannelRequest,PendingOpenChannel,ActiveOpenChannel,DecodedInvoice,PayInvoiceRequest
 from result import Result, Ok, Err
+
 
 class Lnd(BitcoinLightingNode):
     def __init__(self) -> None:
@@ -23,9 +24,9 @@ class Lnd(BitcoinLightingNode):
             creds.grpc_host, combined_credentials, channel_options)
         self.stub = lnrpc.LightningStub(grpc_channel)
         self.routerstub = routerrpc.RouterStub(grpc_channel)
-        
+
     @staticmethod
-    def get_credentials(tls_cert_path: str, macaroon_path: str) -> Any:
+    def get_credentials(tls_cert_path: str, macaroon_path: str) -> grpc.ChannelCredentials:
         tls_certificate = open(tls_cert_path, 'rb').read()
         ssl_credentials = grpc.ssl_channel_credentials(tls_certificate)
         macaroon = codecs.encode(open(macaroon_path, 'rb').read(), 'hex')
@@ -35,50 +36,100 @@ class Lnd(BitcoinLightingNode):
             ssl_credentials, auth_credentials)
         return combined_credentials
 
-    def open_channel(self) -> Result[None, str]:
+    def open_channel(self,channel: OpenChannelRequest) -> Result[None, str]:
         """BitcoinLightingNode"""
-        return Ok(None)
-    
-    def close_channel(self) -> Result[None, str]:
+        channel_point = None
+        result = self.is_peer_with(channel.peer_pubkey)
+        if isinstance(result,Ok):
+            is_peers_with = result.value
+            if not is_peers_with:
+                self.add_peer(channel.peer_pubkey, channel.peer_host)
+            try:
+                channel_point = self.stub.OpenChannelSync(channel)
+            except grpc._channel._InactiveRpcError as e:
+                return Err(e.debug_error_string())
+        return Ok(channel_point)
+
+    def is_peer_with(self,pubkey:str) -> Result[bool,str]:
+        list_peers_response:ln.ListPeersResponse = self.stub.ListPeers(ln.ListPeersRequest())
+        peers = list_peers_response.peers
+        return Ok(pubkey in peers)
+
+    def add_peer(self, pubkey, address):
+        ln_addr = ln.LightningAddress(pubkey=pubkey, host=address)
+        connect_request = ln.ConnectPeerRequest(addr=ln_addr)
+        try:
+            res:ln.ConnectPeerResponse = self.stub.ConnectPeer(connect_request)
+        except grpc._channel._InactiveRpcError as e:
+            return Err(e.debug_error_string())
+        return Ok(res)
+
+    def close_channel(self,close_channel_req:CloseChannelRequest) -> Result[ln.ClosedChannelsResponse, str]:
         """BitcoinLightingNode"""
-        return Ok(None)
-    
-    def get_pending_channels(self) -> Result[Box, str]:
+        channel_point = close_channel_req.channel_point
+        sat_per_vbyte = close_channel_req.vbyte_sats
+        if (not channel_point) or (not sat_per_vbyte):
+            return Err("Must provide chan_id and sat_per_vbyte to close a channel.")
+        open_channels_res = self.get_opened_channels()
+        if isinstance(open_channels_res,Ok):
+            open_channels = open_channels_res.unwrap()
+            target_channels = list(filter(lambda channel: channel.channel_point == channel_point,open_channels))
+            if not len(target_channels) > 0:
+                return Err(f"The channel id provided does not exist:  {channel_point}")
+            target_channel = target_channels[0]
+            channel_point_str = target_channel.channel_point
+            funding_txid_str, output_index = channel_point_str.split(':')
+
+            close_channel_request = ln.CloseChannelRequest(
+                channel_point=ln.ChannelPoint(
+                    funding_txid_str=funding_txid_str, output_index=int(output_index)),
+                sat_per_vbyte=sat_per_vbyte,
+                force=close_channel_req.is_force,
+            )
+            close_status_update_response = self.stub.CloseChannel(close_channel_request)
+            return Ok(close_status_update_response)
+        else:
+            return open_channels_res
+    def get_pending_open_channels(self) -> Result[List[PendingOpenChannel], str]:
         """BitcoinLightingNode"""
-        return 
-    
-    def get_opened_channels(self) -> Result[Box, str]:
+        pending_channels_request = ln.PendingChannelsRequest()
+        pending_channels_response:ln.PendingChannelsResponse = self.stub.PendingChannels(pending_channels_request)
+        pending_open_channels = pending_channels_response.pending_open_channels
+        return Ok(list(map(lambda x: x.channel, pending_open_channels)))
+
+    def get_opened_channels(self) -> Result[List[ActiveOpenChannel], str]:
         """BitcoinLightingNode"""
-        return
-    
+        request = ln.ListChannelsRequest()
+        response: ln.ListChannelsResponse = self.stub.ListChannels(request)
+        return Ok(response.channels)
+
     def get_invoice(self, sats: int) -> Result[str, str]:
         """BitcoinLightingNode"""
         invoice = ''
         raise NotImplementedError
         return Ok(invoice)
-    
-    def pay_invoice(self) -> Result[None, str]:
+
+    def pay_invoice(self,pay_invoice_req: PayInvoiceRequest) -> Result[None, str]:
         """BitcoinLightingNode"""
         return Ok(None)
-    
+
     def get_address(self) -> Result[str, str]:
         """BitcoinLightingNode"""
         new_address_request = ln.NewAddressRequest(type=5) # get unused taproot pubkey type
         new_address_response = self.stub.NewAddress(new_address_request)
         #TODO self.log.info("LND generated deposit address: {}".format(addr)
         return Ok(new_address_response.address)
-    
+
     def send_onchain(self) -> Result[None, str]:
         """BitcoinLightingNode"""
         raise NotImplementedError
         return Ok(None)
-    
+
     def get_unconfirmed_balance(self) -> Result[int, str]:
         """BitcoinLightingNode"""
         total = 0
         txs = self.get_txns(end_height=-1).transactions
         txns = list(filter(lambda x: x.num_confirmations == 0, txs))
-        txns = self.get_unconfirmed_txns()
         if len(txns) > 0:
             for tx in txns:
                 total += tx.amount
@@ -91,19 +142,19 @@ class Lnd(BitcoinLightingNode):
         balance_response = self.stub.WalletBalance(balance_request)
         confirmed = int(balance_response.confirmed_balance)
         return Ok(confirmed)
-    
-    def decode_invoice(self, invoice: str) -> Result[Box, str]:
+
+    def decode_invoice(self, invoice: str) -> Result[DecodedInvoice, str]:
         """BitcoinLightingNode"""
         request = ln.PayReqString(pay_req=invoice)
         decoded = self.stub.DecodePayReq(request)
         # TODO return DecodedInvoice class instance
         return 
-    
+
     def sign_message(self, message: str) -> Result[str, str]:
         """BitcoinLightingNode"""
         signed_message = ''
         return Ok(signed_message)
-    
+
     def get_alias(self, pubkey: str) -> Result[str, str]:
         """BitcoinLightingNode"""
         alias = self.stub.GetNodeInfo(ln.NodeInfoRequest(pub_key=pubkey)).node.alias
@@ -113,17 +164,6 @@ class Lnd(BitcoinLightingNode):
 """
 BELOW IS OLD lnd.py FOR REFERENCE
 """
-    def add_peer(self, pubkey, address):
-        ln_addr = ln.LightningAddress(pubkey=pubkey, host=address)
-        connectRequest = ln.ConnectPeerRequest(addr=ln_addr)
-        res = None
-        try:
-            res = self.stub.ConnectPeer(connectRequest)
-            # TODO self.log.info(f"LND connected to peer {pubkey}@{address}")
-        except grpc._channel._InactiveRpcError as e:
-            pass
-            # TODO self.log.notify(f"An error occurred while adding peer: {e}")
-        return res
 
     def get_graph(self, refresh=False):
         if self.graph is None or refresh:
@@ -179,43 +219,6 @@ BELOW IS OLD lnd.py FOR REFERENCE
         send_response: dict[Any, Any] = self.stub.SendCoins(send_request)
         return send_response
 
-    def open_channel(self, channel: ln.openChannelRequest) -> str:
-        channel_point = None
-        if not self.is_peer_with(channel.node_pubkey):
-            self.add_peer(channel.node_pubkey, channel.address)
-        try:
-            channel_point = self.stub.OpenChannelSync(channel)
-        except grpc._channel._InactiveRpcError as e:
-            if "Number of pending channels exceed maximum" in e.debug_error_string():
-                return channel_point  # done for now
-            else:
-                pass
-                #TODO self.log.notify(f"An error occurred while opening channel: {e}")
-        return channel_point
-
-    def close_channel(self, chan_id, sat_per_vbyte, force=False, target_conf=None, delivery_address=None):
-        if (not chan_id) or (not sat_per_vbyte):
-            # TODO self.log.info(f"Must provide chan_id and sat_per_vbyte to close a channel. " f"chan_id: {chan_id}, sat_per_vbyte: {sat_per_vbyte}")
-            return
-        target_channels = list(
-            filter(lambda channel: channel.chan_id == chan_id, self.get_open_channels()))
-        if not len(target_channels) > 0:
-            # TODO self.log.info(f"The channel id provided does not exist:  {chan_id}")
-            return
-        target_channel = target_channels[0]
-        channel_point_str = target_channel.channel_point
-        funding_txid_str, output_index = channel_point_str.split(':')
-
-        close_channel_request = ln.CloseChannelRequest(
-            channel_point=ln.ChannelPoint(
-                funding_txid_str=funding_txid_str, output_index=int(output_index)),
-            sat_per_vbyte=sat_per_vbyte,
-            force=force,
-            target_conf=target_conf,
-            delivery_address=delivery_address)
-        close_status_update_response = self.stub.CloseChannel(
-            close_channel_request)
-        return close_status_update_response
 
     def get_onchain_balance(self) -> int:
         balance_request = ln.WalletBalanceRequest()
@@ -256,11 +259,3 @@ BELOW IS OLD lnd.py FOR REFERENCE
                 total += tx.amount
         #TODO self.log.info("LND unconfirmed balance: {} sats".format(total))
         return total
-
-    def get_pending_channels(self) -> list[Any]:
-        pending_channels_request = ln.PendingChannelsRequest()
-        pending_channels_response = self.stub.PendingChannels(
-            pending_channels_request)
-        pending_open_channels = pending_channels_response.pending_open_channels
-        return list(
-            map(lambda x: x.channel, pending_open_channels))
